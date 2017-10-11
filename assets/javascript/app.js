fbd99a69db548780446d5df5b19df88075e32d62
Kayvan Rahmani <kayvanrahmani@Kayvans-MacBook-Air.local>
2017-10-11 13:14:22 -0500

    finished all jquery

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,7 @@ var intervalId;
 
 var questions = ["What year were the Chicago Bulls formed?", "Who was the bulls first pick of the 2011 draft?", "Who is the Bulls all time leading scorer" ];
 
-var answers = ["1966", "Derrick Rose", "Michael Jordan", "72"];
+var answers = ["1966", "Derrick Rose", "6", "72", "Michael Jordan"];
 
 var userGuess = [];
 
@@ -21,47 +21,85 @@ var div = $("<div></div>");
 
 $(".comeBack").hide();
 $("#show-number").hide();
+$(".results").hide();
 
  function run() {
       intervalId = setInterval(decrement, 1000);
     }
 
-    
     function decrement() {
-
      
       number--;
-
      
-      $("#show-number").html("<h2>" + number + "</h2>");
-
+      $("#show-number").html("<h2> Seconds Remaining: " + number + "</h2>");
 
      
       if (number === 0) {
-
-     
         
 
-        alert("nice try kenny")
-        //results();
+        results();
+        
       }
     }
 
-$("button").on("click", function() {
+$("#start").on("click", function() {
         $("#firstToGo").hide(1000);
         run();
         $(".comeBack").show(1000);
        
         $("#show-number").show();
 
-
-
-
-
 });
 
+$("#results-main").on("click", function(){
+  results();
+});
+
+function results (){
+clearInterval(intervalId);
+$(".comeBack").hide(1000);
+
+    userGuess[0] = $('input:radio[name="que0"]:checked').val();
+    console.log(userGuess[0]);
+    userGuess[1] = $('input:radio[name="que1"]:checked').val();
+    console.log(userGuess[1]);
+    userGuess[2] = $('input:radio[name="que2"]:checked').val();
+    userGuess[3] = $('input:radio[name="que3"]:checked').val();
+    userGuess[4] = $('input:radio[name="que4"]:checked').val();
+    for(var i=0; i<userGuess.length; i++){
+    
+    
+      if(userGuess[i] === answers[i]){
+       correctAnswers++;
+       console.log(correctAnswers);
+      } else if(userGuess[i] == undefined){
+        unanswered++;
+      } else {
+        wrongAnswers++;
+       }
+
+    }
+      $("#correct").html("Number of questions you got correct: " + correctAnswers);
+      $("#wrong").html("Number of questions you got wrong: " + wrongAnswers);
+      $("#unanswered").html("Number of questions unanswered: " + unanswered);
+      $(".results").show();
+    }
 
 
- 
+
+
+
+/*$(document).on("click","input[name=que1]:radio",function(){
+    userGuess = $("input:radio[name=que1]:checked").val();
+    if (userGuess == "Derrick Rose") {
+      correctAnswers++;
+      $("#correct").html(correctAnswers);
+    }else if (userGuess !== "Derrick Rose") {
+     wrongAnswers--;
+     $("#wrong").html(wrongAnswers);
+    }else {
+      unanswered++;
+      $("#unanswered").html(unanswered);
+    }
+ */
       });
- 
