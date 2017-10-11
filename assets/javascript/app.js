6a2e3d2fa82cfd84ec04d26d404a0d8bf43da06c
Kayvan Rahmani <kayvanrahmani@Kayvans-MacBook-Air.local>
2017-10-11 16:57:30 -0500

    minor editing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-
+//varibles
 var intervalId;
 
 var questions = ["What year were the Chicago Bulls formed?", "Who was the bulls first pick of the 2011 draft?", "Who is the Bulls all time leading scorer" ];
@@ -19,46 +19,55 @@ var unanswered = 0;
 
 var div = $("<div></div>");
 
+//Hiding questionaire page , results page, and timer
 $(".comeBack").hide();
 $("#show-number").hide();
 $(".results").hide();
 
+//function for timer
  function run() {
+  //Timer decreasing by second
       intervalId = setInterval(decrement, 1000);
     }
 
     function decrement() {
      
       number--;
-     
+     //HTML div to show seconds remaining
       $("#show-number").html("<h2> Seconds Remaining: " + number + "</h2>");
 
      
       if (number === 0) {
         
-
+//When timer hits 0 seconds go to results page
         results();
         
       }
     }
-
+// When start button is clicked...
 $("#start").on("click", function() {
         $("#firstToGo").hide(1000);
+  //Hide page
         run();
+    //perform "run" function 
         $(".comeBack").show(1000);
-       
+       //div's with show function to appear the questions and timer.
         $("#show-number").show();
 
 });
-
+//When submit button is pressed take us to results page
 $("#results-main").on("click", function(){
   results();
 });
 
 function results (){
 clearInterval(intervalId);
+//clearing the timer
 $(".comeBack").hide(1000);
+//hiding the questions sheet when timer expires or submit button is pressed
 
+
+//Setting the guess to the radio button checked on
     userGuess[0] = $('input:radio[name="que0"]:checked').val();
     console.log(userGuess[0]);
     userGuess[1] = $('input:radio[name="que1"]:checked').val();
@@ -66,9 +75,10 @@ $(".comeBack").hide(1000);
     userGuess[2] = $('input:radio[name="que2"]:checked').val();
     userGuess[3] = $('input:radio[name="que3"]:checked').val();
     userGuess[4] = $('input:radio[name="que4"]:checked').val();
+ //For looping guesses  
     for(var i=0; i<userGuess.length; i++){
     
-    
+    //Creating logic for results page
       if(userGuess[i] === answers[i]){
        correctAnswers++;
        console.log(correctAnswers);
@@ -79,6 +89,8 @@ $(".comeBack").hide(1000);
        }
 
     }
+
+    //displaying results 
       $("#correct").html("Number of questions you got correct: " + correctAnswers);
       $("#wrong").html("Number of questions you got wrong: " + wrongAnswers);
       $("#unanswered").html("Number of questions unanswered: " + unanswered);
@@ -89,17 +101,4 @@ $(".comeBack").hide(1000);
 
 
 
-/*$(document).on("click","input[name=que1]:radio",function(){
-    userGuess = $("input:radio[name=que1]:checked").val();
-    if (userGuess == "Derrick Rose") {
-      correctAnswers++;
-      $("#correct").html(correctAnswers);
-    }else if (userGuess !== "Derrick Rose") {
-     wrongAnswers--;
-     $("#wrong").html(wrongAnswers);
-    }else {
-      unanswered++;
-      $("#unanswered").html(unanswered);
-    }
- */
       });
